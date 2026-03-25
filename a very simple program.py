25df9e2dac6f11dc2874c54b15908b3fb98625ad
Ashmit2013 <hiranmoybanerjee1940@gmail.com>
2026-03-25 21:43:57 +0530

    Add files via upload

---
+++ a very simple program.py
@@ -0,0 +1,11 @@
+print("Enter Your Marks Obtained in 4 Subjects: ")
+
+
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+हिंंदी = int(input("hindi :"))
+sum = math+english+science+हिंंदी
+print("the sum of math, english, science and हिंंदी is:", sum)
+percent = (sum/400)*100
+print("your percentage is:", percent)
